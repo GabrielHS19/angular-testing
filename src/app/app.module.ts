a68ff36db3e71d9b940b2d94456cd12dd3afb5da
GabrielHS19 <jesus.gh72@gmail.com>
2020-10-09 22:07:20 -0500

    add currencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CurrenciesDemoComponent } from './currencies-demo/currencies-demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrenciesDemoComponent
   ],
   imports: [
     BrowserModule,
